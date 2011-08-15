ee33e6c4b2e00b2419b9662283bdfac4925972f4
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-08-15 13:33:38 +1200

    Minor tweak.

--- run.sh
+++ run.sh
@@ -5,15 +5,15 @@ NRAMPUPS=5
 
 #sequential_micro="gcd fib matrix queens regex sorter codejam_0511A codejam_0511B"
 
-sequential_micro="gcd queens regex sorter codejam_0511A codejam_0511B"
+sequential_micro="gcd fib queens regex sorter codejam_0511A codejam_0511B"
 
 for benchmark in $sequential_micro
 do
     echo $benchmark
     echo "================================================"
     echo -n "Java:   "
-    java -cp ".:$WHILEY_HOME/lib/wyrt.jar:sequential/micro/$benchmark" Runner -r$NRAMPUPS -n$NRUNS JavaMain sequential/micro/$benchmark/small.in
+    java -server -cp ".:$WHILEY_HOME/lib/wyrt.jar:sequential/micro/$benchmark" Runner -r$NRAMPUPS -n$NRUNS JavaMain sequential/micro/$benchmark/small.in
     echo -n "Whiley: "
-    java -cp ".:$WHILEY_HOME/lib/wyrt.jar:sequential/micro/$benchmark" Runner -r$NRAMPUPS -n$NRUNS Main sequential/micro/$benchmark/small.in
+    java -server -cp ".:$WHILEY_HOME/lib/wyrt.jar:sequential/micro/$benchmark" Runner -r$NRAMPUPS -n$NRUNS Main sequential/micro/$benchmark/small.in
     echo ""
 done
