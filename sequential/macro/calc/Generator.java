8b1d3122b3f5865c2c9549fa705ae211e4cccbdf
DavePearce <david.pearce@ecs.vuw.ac.nz>
2011-11-03 17:01:04 +1300

    Making some kind of progress ...

--- sequential/macro/calc/Generator.java
+++ sequential/macro/calc/Generator.java
@@ -1,14 +1,52 @@
+import java.util.*;
+
 public class Generator {
 
-    public static final N = 100;
+    public static final int N = 10;
+    public static String[] vars = {
+	"p","q","r","s","t","u","v","w","x","y","z"
+    };
+    
 
     public static void main(String[] args) {
-	for(i=0;i!=N;++i) {
-	    if(Math.random() > 0.5) {
+	Random random = new Random();
+
+	// initialise all variables
+	for(int i=0;i!=vars.length;++i) {
+		System.out.print("set ");
+		System.out.print(vars[i]);
+		System.out.println(" 0");
+	}
+	for(int i=0;i!=N;++i) {
+	    if(random.nextDouble() > 0.75) {
 		// generate set statement
-		System.out.println("set")
-	    } else {
-		System.out.println("print")		
+		System.out.print("set ");
+		System.out.print(vars[random.nextInt(11)]);
+		System.out.print(" ");
+		System.out.println(random.nextInt(Integer.MAX_VALUE));
+	    } else {		
+		System.out.print("print ");
+		int nterms = random.nextInt(100);
+		for(int j=0;j!=nterms;++j) {
+		    if(j != 0) {
+			switch(random.nextInt(4)) {
+			case 0:
+			    System.out.print("+");
+			    break;
+			case 1:
+			    System.out.print("-");
+			    break;
+			case 2:
+			    System.out.print("*");
+			    break;
+			case 3:
+			    System.out.print("/");
+			    break;
+			}
+		    }	
+		    System.out.print(vars[random.nextInt(11)]);
+		}
+		System.out.println();
 	    }
 	}
     }
