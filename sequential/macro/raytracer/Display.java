c2d1ae923755916f9081ab0ab5ae0ad992cf2c20
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-02-08 19:18:44 +1300

    The display is now basically working.

--- sequential/macro/raytracer/Display.java
+++ sequential/macro/raytracer/Display.java
@@ -6,6 +6,7 @@ import java.awt.Image;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
 import java.math.BigInteger;
 
 import javax.swing.JFrame;
@@ -17,71 +18,50 @@ import javax.swing.JFrame;
  */
 public class Display extends Canvas {
     
-	private static final int WIDTH = 1024;
-	private static final int HEIGHT = 1024;
+	private static final int WIDTH = 512;
+	private static final int HEIGHT = 512;
 	
-	public Display(JFrame frame) {
-		this.setSize(WIDTH, HEIGHT);			
-		this.requestFocus();		
-    }
-    
-	public void paint(Graphics g) {
-		
+	private static BufferedImage offscreen = null;
+	public void paint(Graphics g) {	
+		// transfer offscreen to window
+		g.drawImage(offscreen, 0, 0, this);
 	}
 
+	private static void initialiseOffscreen() {
+		offscreen = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+		// clear the exposed area
+		Graphics offgc = offscreen.getGraphics();
+		offgc.setColor(Color.WHITE);
+		offgc.fillRect(0, 0, WIDTH, HEIGHT);		
+	}
+	
 	private static JFrame frame;
 	private static Display display;
 
 	public static void draw(BigInteger x, BigInteger y, BigInteger r, BigInteger g, BigInteger b) {
+		draw(x.intValue(),y.intValue(),r.byteValue(),g.byteValue(),b.byteValue());
+	}
+	
+	public static void draw(int x, int y, byte r, byte g, byte b) {	
 		if(display == null) {
 			frame = new JFrame("Raytracer Display");
-			display = new Display(frame);
-			frame.setSize(500, 500);
+			display = new Display();
+			display.setSize(WIDTH,HEIGHT);
+			frame.add(display);
+			frame.setSize(WIDTH,HEIGHT);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.pack();
 			frame.setVisible(true);
+			initialiseOffscreen();
 		}
+		int rgb = (r << 16) | (g << 8) | (b);
+		offscreen.setRGB(x, y, rgb);
+		display.repaint();
 	}    
 	
-	public void paint(Graphics g) {
-		int width = WIDTH;
-		int height = HEIGHT;
-		g.drawImage(offscreen, 0);
-		g.draw
-	}
-	
-	private Image offscreen = null;
-	public void update(Graphics g) {	
-		if(offscreen == null) {
-			initialiseOffscreen();			
-		} 
-		Image localOffscreen = offscreen;
-		Graphics offgc = offscreen.getGraphics();		
-		// do normal redraw
-		paint(offgc);
-		// transfer offscreen to window
-		g.drawImage(localOffscreen, 0, 0, this);
-	}
-
-	private void initialiseOffscreen() {
-		Dimension d = size();
-		offscreen = createImage(d.width, d.height);
-		// clear the exposed area
-		Graphics offgc = offscreen.getGraphics();
-		offgc.setColor(getBackground());
-		offgc.fillRect(0, 0, d.width, d.height);
-		offgc.setColor(getForeground());
-		
-		// First, draw the board
-		
-		for(int x=0;x!=WIDTH;++x) {
-			for(int y=0;y!=HEIGHT;++y) {
-				
-			}
-		}
-		
-	}
-	
 	public static void main(String[] args) {
-		draw(null,null,null,null,null);
+		for(int i=10;i!=100;++i) {
+			draw(i,i,(byte) 100, (byte) 100, (byte) 100);
+		}
 	}
 }
