e55c0c92f1562ba9c296addf81ad79703885e2ca
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-04-19 18:47:13 +1200

    Added the GUI code

---
+++ src/107_minesweeper/GameCanvas.java
@@ -0,0 +1,135 @@
+import java.io.IOException;
+import java.awt.*;
+import javax.imageio.ImageIO;
+
+/**
+ * <p>
+ * Implements the battle area in which robots are drawn. This is implement as an
+ * extension of java.awt.Canvas, and robots are drawn directly onto this as
+ * images. The background is constructed by drawing a sequence of filled squares
+ * with different colours.
+ * </p>
+ * 
+ * <p>
+ * The battle area uses "interpolation" to give the robots smoother movement
+ * from one square to another. The idea behind interpolation is that we plot the
+ * line from the current position to the next position and then draw the robot
+ * at different positions along that line. This stops the robot from seemingly
+ * "jumping" from one location to another.
+ * </p>
+ * 
+ * @author David J. Pearce
+ * 
+ */
+public class GameCanvas extends Canvas {
+
+	/**
+	 * The image path simply determines where images are stored relative to this
+	 * class.
+	 */
+	private static final String IMAGE_PATH = "images/";
+
+	/**
+	 * The square width constant determines the width (in pixels) of a square in
+	 * the battle area. Changing this constant should automatically reshape the
+	 * display. The optimal size is 24x24, based on the dimensions of the images
+	 * used for the robots.
+	 */
+	private static final int SQUARE_WIDTH = 24;
+
+	/**
+	 * The square height constant determines the height (in pixels) of a square
+	 * in the battle area. Changing this constant should automatically reshape
+	 * the display. The optimal size is 24x24, based on the dimensions of the
+	 * images used for the robots.
+	 */
+	private static final int SQUARE_HEIGHT = 24;
+
+	/**
+	 * Represents the image of a robot which is now dead. That is, where the
+	 * isDead flag is true.
+	 */
+	//private static Image deadBot = loadImage("DeadRobot.png");
+
+	/**
+	 * Width of the board in squares
+	 */
+	private int width;
+	
+	/**
+	 * Height of the board in squares
+	 */
+	private int height;
+	
+	/**
+	 * Construct a canvas to visually display a given robot battle.
+	 * 
+	 * @param battle
+	 */
+	public GameCanvas(int width, int height) {
+		
+		setBounds(0, 0, width * SQUARE_WIDTH, height * SQUARE_HEIGHT);
+	}
+
+	/**
+	 * Paint the given battle onto the given graphics object.
+	 */
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		int width = getWidth();
+		int height = getHeight();
+
+		// Draw the background of the display as a rectangle of all white.
+		g2d.setColor(Color.WHITE);
+		g2d.fillRect(0, 0, width, height);
+
+		// Draw alternating boxes of light gray to give a checker-board display,
+		// making it easier to distinguish the individual squares of the board.
+		g2d.setColor(Color.LIGHT_GRAY);
+		for (int x = 0; x < width; x = x + 1) {
+			for (int y = 0; y < height; y = y + 1) {
+				if ((x + y) % 2 == 0) {
+					int xp = x * SQUARE_WIDTH;
+					int yp = y * SQUARE_HEIGHT;
+					g2d.fillRect(xp, yp, SQUARE_WIDTH, SQUARE_HEIGHT);
+				}
+			}
+		}		
+	}
+
+	/**
+	 * An offscreen buffer used to reduce flicker between frames.
+	 */
+	private Image offscreen = null;
+
+	public void update(Graphics g) {
+		int width = getWidth();
+		int height = getHeight();
+		if (offscreen == null || offscreen.getWidth(this) != width
+				|| offscreen.getHeight(this) != height) {
+			offscreen = createImage(width, height);
+		}
+		Image localOffscreen = offscreen;
+		Graphics2D offgc = (Graphics2D) offscreen.getGraphics();
+		// do normal redraw
+		paint(offgc);
+		// transfer offscreen to window
+		g.drawImage(localOffscreen, 0, 0, this);
+	}
+
+	private static Image loadImage(String filename) {
+		// using the URL means the image loads when stored
+		// in a jar or expanded into individual files.
+		java.net.URL imageURL = GameCanvas.class.getResource(IMAGE_PATH
+				+ filename);
+
+		try {
+			Image img = ImageIO.read(imageURL);
+			return img;
+		} catch (IOException e) {
+			// we've encountered an error loading the image. There's not much we
+			// can actually do at this point, except to abort the game.
+			throw new RuntimeException("Unable to load image: " + filename);
+		}
+	}
+}
---
+++ src/107_minesweeper/GameFrame.java
@@ -0,0 +1,51 @@
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+/**
+ * Implements the outer window of the Robot War game. This includes any buttons,
+ * the window frame itself and its title.
+ * 
+ * @author David J. Pearce
+ * 
+ */
+public class GameFrame extends JFrame implements ActionListener {
+	private JPanel centerPanel;
+	private GameCanvas canvas;
+
+	public GameFrame(int width, int height) {
+		super("Minewseeper");
+
+		canvas = new GameCanvas(width,height);
+		centerPanel = new JPanel();
+		centerPanel.setLayout(new BorderLayout());
+		Border cb = BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(3, 3, 3, 3),
+				BorderFactory.createLineBorder(Color.gray));
+		centerPanel.setBorder(cb);		
+		centerPanel.add(canvas, BorderLayout.CENTER);
+		
+		add(centerPanel, BorderLayout.CENTER);
+		
+		setFocusable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		new GameFrame(10,10);
+	}
+}
