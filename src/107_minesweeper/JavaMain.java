1cea44f0b9b0038137353cd19b416990bc6d8bae
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-04-26 20:33:49 +1200

    107_minesweeper: refactored the Java main class to avoid collision with the Whiley main class.

--- src/107_minesweeper/JavaMain.java
+++ src/107_minesweeper/JavaMain.java
@@ -1,4 +1,4 @@
-public class Main {	
+public class JavaMain {	
     public static void main(String[] args) {
 	BoardAdaptor b = new BoardAdaptor(10,10,10);
 	GraphicalUserInterface f = new GraphicalUserInterface(b);
